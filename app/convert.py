7ae31738bf7051a3a96400a2af415f07cd407658
Spitfire777 <herotimer777@gmail.com>
2025-09-18 11:45:53 -0600

    new file:   app/__pycache__/convert.cpython-312.pyc 	new file:   app/convert.py 	new file:   app/install.py 	modified:   main.py

---
+++ app/convert.py
@@ -0,0 +1,97 @@
+import configparser
+import os
+
+def create_inf_file(name, file, output_path: str):
+    
+    o_file = output_path if output_path.endswith('.inf') else os.path.join(output_path, 'install.inf')
+
+    # Ensure output path is a directory if it doesn't end with .inf
+    if not os.path.exists(o_file) and not o_file.endswith('.inf'):
+        os.makedirs(o_file)
+    
+    print(f"Output directory set to: {o_file}")
+
+    # Read the cursor scheme configuration file
+    with open(file, 'r', encoding='utf-8-sig') as f:
+        content = f.read()
+    cur_file = configparser.ConfigParser()
+    cur_file.read_string(content)
+
+    cur_dir = {}
+    for cursor in cur_file.sections():
+        for _, value in cur_file.items(cursor):
+            cur_dir[cursor] = value
+
+    # Checking and adding cursor directories, files and associations if they exist for the Registry
+    
+    dirs, cursors, files = {}, [], []
+    for _ in range(17): cursors.append(None)
+    order = ['pointer', 'help', 'work', 'busy', 'cross', 'text', 'hand', 'unavailiable',
+             'vert', 'horz', 'dgn1', 'dgn2', 'move', 'alternate', 'link', 'pin', 'person']
+
+    for key in cur_dir:
+        files.append(cur_dir[key])
+        type = ""
+        match key:
+            case 'Arrow':       type = 'pointer'
+            case 'Help':        type = 'help'
+            case 'AppStarting': type = 'work'
+            case 'Wait':        type = 'busy'
+            case 'Crosshair':   type = 'cross'
+            case 'IBeam':       type = 'text'
+            case 'NWPen':       type = 'hand'
+            case 'No':          type = 'unavailiable'
+            case 'SizeNS':      type = 'vert'
+            case 'SizeWE':      type = 'horz'
+            case 'SizeNWSE':    type = 'dgn1'
+            case 'SizeNESW':    type = 'dgn2'
+            case 'SizeAll':     type = 'move'
+            case 'UpArrow':     type = 'alternate'
+            case 'Hand':        type = 'link'
+            case 'Pin':         type = 'pin'
+            case 'Person':      type = 'person'
+            case _: print(f"Warning: Unknown cursor type '{key}' found in configuration.")
+        
+        print(f"Mapping cursor '{key}' to file '{cur_dir[key]}' as type '{type}'")
+        if type in order:
+            print(f"Found cursor type '{type}' at index {order.index(type)}")
+            dirs[type] = r'%10%\%CUR_DIR%\%'+type+'%'
+            cursors[order.index(type)] = cur_dir[key]
+        else:
+            print(f"Cursor type '{type}' not in predefined order list.")
+        type = ""
+
+    main_header = 'HKCU,"Control Panel\\Cursors\\Schemes","%SCHEME_NAME%",,"'
+    key_list = ""
+    for ord in order:
+        if ord in dirs.keys():
+            key_list += dirs[ord] + ','
+    scheme_value = main_header + key_list.rstrip(",") + '"'
+    cur_txt = r'10,"%CUR_DIR%"'
+
+    # Prepare INF installer configuration
+    inf_installer = configparser.ConfigParser(allow_no_value=True, interpolation=None)
+    inf_installer.optionxform = str # type: ignore # Preserve case sensitivity
+    inf_installer['Version'] = {
+        'Signature': '$CHICAGO$',}
+    inf_installer['DefaultInstall'] = {
+        'CopyFiles': 'Scheme.Cur,',
+        'AddReg': 'Scheme.Reg'}
+    inf_installer['DestinationDirs'] = {
+        'Scheme.Cur': cur_txt,
+        'Scheme.Txt': cur_txt}
+    inf_installer['Scheme.Reg'] = { # type: ignore
+        scheme_value: None}
+    inf_installer['Scheme.Cur'] = { # type: ignore
+        f'"{f}"': None for f in files}
+    inf_installer['Strings'] = { 
+        'CUR_DIR': name,
+        'SCHEME_NAME': name,
+        **{k : v for k, v in zip(order, cursors) if v is not None}
+    }
+
+    # Write the INF file
+    with open(o_file, 'w', encoding='utf-8') as inf_file:
+        inf_installer.write(inf_file)
+    
+    return o_file
--- main.py
+++ main.py
@@ -1,129 +1,63 @@
-import sys, configparser
+import sys
+import app.convert as convert
+import os
 
-def create_inf_file(name, file, verbose=False, only_convert=False, output_path=""):
-            
-    # Read the cursor scheme configuration file
-    file = sys.argv[1]
-    with open(file, 'r', encoding='utf-8-sig') as f:
-        content = f.read()
-    cur_file = configparser.ConfigParser()
-    cur_file.read_string(content)
-
-    cur_dir = {}
-    for cursor in cur_file.sections():
-        for _, value in cur_file.items(cursor):
-            cur_dir[cursor] = value
-
-    # Prepare INF installer configuration
-    inf_installer = configparser.ConfigParser(allow_no_value=True)
-    inf_installer['Version'] = {
-        'Signature': '$CHICAGO$',}
-    inf_installer['DefaultInstall'] = {
-        'CopyFiles': 'Scheme.Cur,',
-        'AddReg': 'Scheme.Reg'}
-    inf_installer['DestinationDirs'] = {
-        'Scheme.Cur': '10,"%%CUR_DIR%%"',
-        'Scheme.Txt': '10,"%%CUR_DIR%%"'}
-    
-
-    # Checking and adding cursor directories, files and associations if they exist for the Registry
-    main_header = 'HKCU,"Control Panel\\Cursors\\Schemes","%SCHEME_NAME%",'
-    dirs, files = [], []
-    cursors = {}        
-
-    for key in cur_dir:
-        files.append(cur_dir[key]['Path'])
-        match key:
-            case 'Arrow':
-                dirs.append(r'%10%\%CUR_DIR%\%pointer%')
-                cursors['pointer'] = cur_dir[key]['Path']
-            case 'Help':
-                dirs.append(r'%10%\%CUR_DIR%\%help%')
-                cursors['help'] = cur_dir[key]['Path']
-            case 'AppStarting':
-                dirs.append(r'%10%\%CUR_DIR%\%work%')
-                cursors['work'] = cur_dir[key]['Path']
-            case 'Wait':
-                dirs.append(r'%10%\%CUR_DIR%\%busy%')
-                cursors['busy'] = cur_dir[key]['Path']
-            case 'Crosshair':
-                dirs.append(r'%10%\%CUR_DIR%\%cross%')
-                cursors['cross'] = cur_dir[key]['Path']
-            case 'IBeam':
-                dirs.append(r'%10%\%CUR_DIR%\%text%')
-                cursors['text'] = cur_dir[key]['Path']
-            case 'NWPen':
-                dirs.append(r'%10%\%CUR_DIR%\%hand%')
-                cursors['hand'] = cur_dir[key]['Path']
-            case 'No':
-                dirs.append(r'%10%\%CUR_DIR%\%unavailiable%')
-                cursors['unavailiable'] = cur_dir[key]['Path']
-            case 'SizeNS':
-                dirs.append(r'%10%\%CUR_DIR%\%vert%')
-                cursors['vert'] = cur_dir[key]['Path']
-            case 'SizeWE':
-                dirs.append(r'%10%\%CUR_DIR%\%horz%')
-                cursors['horz'] = cur_dir[key]['Path']
-            case 'SizeNWSE':
-                dirs.append(r'%10%\%CUR_DIR%\%dgn1%')
-                cursors['dgn1'] = cur_dir[key]['Path']
-            case 'SizeNESW':
-                dirs.append(r'%10%\%CUR_DIR%\%dgn2%')
-                cursors['dgn2'] = cur_dir[key]['Path']
-            case 'SizeAll':
-                dirs.append(r'%10%\%CUR_DIR%\%move%')
-                cursors['move'] = cur_dir[key]['Path']
-            case 'UpArrow':
-                dirs.append(r'%10%\%CUR_DIR%\%alternate%')
-                cursors['alternate'] = cur_dir[key]['Path']
-            case 'Hand':
-                dirs.append(r'%10%\%CUR_DIR%\%link%')
-                cursors['link'] = cur_dir[key]['Path']
-            case _:
-                print(f"Warning: Unknown cursor type '{key}' found in configuration.")
-
-    scheme_value = main_header + '"' + ",".join(d for d in dirs) + '"'
-    print(scheme_value)
+def _help():
+    print("Usage: python main.py <filename> [options]\n"
+          "\n"
+          "Options:\n"
+          "  -h, --help                 Show this help message and exit\n"
+          "  -v, --version              Show program version and exit\n"
+          "  -n, --name <name>          Specify the cursor scheme name\n"
+          "  -o, --output <dir|file>    Specify the output directory/file for the INF file\n"
+          )
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python main.py <filename> [options]")
+        _help()
         sys.exit(1)
     
     verbose, only_convert = False, False
-    c_scheme_name, output_path = "" , ""
+    c_scheme_name = "Custom Scheme"
+    file = sys.argv[1]
+    o_dir = os.path.dirname(file)
     args = sys.argv[2:]
     
-    for arg in args:
-        if add_name:
-            c_scheme_name = arg
-            add_name = False
-            continue    
+    for arg in args:   
         match arg:
             case '-h' | '--help':
-                print("Usage: python main.py <filename>")
+                _help()
                 sys.exit(0)
             case "-v" | "--version":
-                print("Cursor Scheme Converter Version 1.0")
+                print("CRS to INF Converter Version 1.0")
                 sys.exit(0)
             case "-n" | "--name":
                 if len(args) > args.index(arg) + 1:
-                    c_scheme_name = args[args.index(arg) + 1]
-            case "-V" | "--verbose":
-                verbose = True
-            case "-c" | "--convert":
-                only_convert = True
+                    arg_index = args.index(arg)
+                    j = 1
+                    for i in args[args.index(arg) + 1:]:
+                        if i.startswith('-'):
+                            break
+                        j += 1
+                    c_scheme_name = " ".join(args[arg_index + 1: arg_index + j])
             case "-o" | "--output":
                 if len(args) > args.index(arg) + 1:
-                    output_path = args[args.index(arg) + 1]
+                    o_dir = args[args.index(arg) + 1]
             case _:
                 if arg.startswith('-'):
                     print(f"Unknown option: {arg}")
                     sys.exit(1)
 
     name = input("Enter cursor scheme name: ") if not c_scheme_name else c_scheme_name
+    
+    print(f"File to convert: {file}"
+          f"\nOutput directory: {o_dir}"
+          f"\nConverting '{file}' with scheme name '{name}'")
+    output_path = convert.create_inf_file(name, file, o_dir)
+    if not only_convert:
+        print(f"INF file '{output_path}' created successfully.")
+        print("To install the cursor scheme, right-click the INF file and select 'Install'.")
 
-    create_inf_file(name, sys.argv(1), )
         
 
 if __name__ == "__main__":
